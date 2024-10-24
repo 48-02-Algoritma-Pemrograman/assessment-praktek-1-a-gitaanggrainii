58a2de9bc7c7c1330abee5210f5ea0a0217b6edb
gitaanggrainii <gitaanggraini2005@gmail.com>
2024-10-24 11:31:56 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Nomor1{
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);{
+            System.out.print("Total Jam Lembur =");
+            int jamLembur = scanner.nextInt();
 
+            double honorLembur;
+
+            if (jamLembur <= 16) {
+                honorLembur = jamLembur * 10000;
+            } else {
+                int jamLemburLebih = jamLembur - 16;
+                honorLembur = (16 * 10000) + (jamLemburLebih * 15000);
+            }
+
+            System.out.println("Honor lembur: " + honorLembur);
+
+        }
     }
 }
